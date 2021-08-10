b7d0dbcb3ff46730da77901e60d5bbc8b450adb4
Gibyoung <38740455+keepbang@users.noreply.github.com>
2021-08-10 19:09:21 +0900

    Create SearchInsertPosition.java

---
+++ src/com/company/leetcode/SearchInsertPosition.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int first = 0;
+        int last = nums.length-1;
+        
+        while(first <= last){
+            int mid = (first + last) / 2;
+            
+            if(nums[mid] == target)
+                return mid;
+            
+            if(nums[mid] < target){
+                first = mid + 1;
+            }else{
+                last = mid - 1;
+            }
+        
+        }
+        
+        return first;
+    }
+    
+}
