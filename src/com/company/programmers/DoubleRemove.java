fb73f230ef8d01f82cd44f9abfc8fac62be87033
Gibyoung <38740455+keepbang@users.noreply.github.com>
2021-08-13 01:57:14 +0900

    Create DoubleRemove.java

---
+++ src/com/company/programmers/DoubleRemove.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution
+{
+    public int solution(String s)
+    {
+
+        if(s.length() < 2 || s.length()%2 != 0){
+            return 0;
+        }
+        
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if(stack.size() == 0){
+                stack.push(s.charAt(i));
+            }else if(stack.peek() == s.charAt(i)){
+                    stack.pop();
+            }else{
+                stack.push(s.charAt(i));
+            }
+        }
+
+        return stack.size() == 0 ? 1 : 0;
+    }
+}
