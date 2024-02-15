e10291b1cd211c68f33ab0bccdd548f674f3f813
keepbang <rlqud1125@gmail.com>
2024-02-15 10:35:43 +0900

    feat: TreeNode 공통화

---
+++ src/com/company/common/TreeNode.java
@@ -0,0 +1,33 @@
+package com.company.common;
+
+import com.company.leetcode.same_tree.SameTree;
+
+/**
+ * create on 2024/02/15.
+ * create by IntelliJ IDEA.
+ *
+ * <p> 클래스 설명 </p>
+ *
+ * @author Gibyoung Chae (keepbang)
+ * @version 1.0
+ * @since 1.0
+ */
+public class TreeNode {
+
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
+
+  TreeNode() {
+  }
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
--- src/com/company/leetcode/same_tree/SameTree.java
+++ src/com/company/leetcode/same_tree/SameTree.java
@@ -1,5 +1,7 @@
 package com.company.leetcode.same_tree;
 
+import com.company.common.TreeNode;
+
 /**
  * create on 2024/01/02.
  * create by IntelliJ IDEA.
@@ -12,27 +14,11 @@ package com.company.leetcode.same_tree;
  */
 public class SameTree {
 
-  class TreeNode {
-
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-      this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-      this.val = val;
-      this.left = left;
-      this.right = right;
-    }
-  }
-
   public boolean isSameTree(TreeNode p, TreeNode q) {
+
+    int x = 4;
+
+    int floor = (int) Math.floor(Math.sqrt(x));
 
     if(p==null && q==null) return true;
 
